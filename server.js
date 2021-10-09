e24ceca3c56c98d8bef1a667c2ff5b1210ce1cee
Aleshatech_Miraz <miraz@aleshatech.com.bd>
2021-10-09 07:30:33 +0600

    Link Connection Modified

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-var express = require("express");
- 
- //use the application off of express.
- var app = express();
- 
- //define the route for "/"
- app.get("/", function (request, response){
-     //show this file when the "/" is requested
-     response.sendFile(__dirname+"/views/index.html");
- });
- 
- //start the server
- app.listen(8080);
- 
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+app.use(express.static("public"));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+//start the server
+server.listen(8080);
